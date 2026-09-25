5b990cf383fbc8563e79cc81642b101c6c1cdf02
ken-masi <ken.masilungan@yahoo.com>
2026-09-25 23:25:31 +0800

    fix another one

--- app/(cashier)/layout.tsx
+++ app/(cashier)/layout.tsx
@@ -176,7 +176,7 @@ export default function CashierLayout({ children }: { children: React.ReactNode 
                {isActive && (
                 <span style={{ position: "absolute", left: 0, top: "20%", bottom: "20%", width: "3px", borderRadius: "0 3px 3px 0", background: "linear-gradient(to bottom, #22c55e, #4ade80)" }} />
               )}
-                <span style={{ color: isActive ? "#6366f1" : "#94a3b8", display: "flex", flexShrink: 0 }}>
+                <span style={{ color: isActive ? "#22c55e" : "#94a3b8", display: "flex", flexShrink: 0 }}>
                   {link.icon}
                 </span>
                 {link.label}
